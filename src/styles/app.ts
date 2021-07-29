60b99ede280f95196eb827c17a599b9d5f5f56df
ramvignesh.manokaran <ramvignesh.manokaran@cognizant.com>
2021-07-29 16:17:30 +0800

    Initial Commit

---
+++ src/styles/app.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      marginTop: 20,
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+  });
