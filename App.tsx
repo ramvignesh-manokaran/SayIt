6240a2bbaf27b0dee89f3d25c87cc0a71bb67b53
ying <zhang_ying@tech.gov.sg>
2021-07-30 15:31:10 +0800

    feat(CameraOverlay) Add camera widget

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {  Text, View } from 'react-native';
-import { styles } from './src/styles/app';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
+import { CameraOverlay } from "./src/components/CameraOverlay";
+import { styles } from "./src/styles/app";
 
 const SayItApp = () => {
   return (
     <View style={styles.container}>
       <Text>Hello SayIt</Text>
       <StatusBar style="auto" />
+      <CameraOverlay></CameraOverlay>
     </View>
   );
-}
+};
 
 export default SayItApp;
-
-
